af1b181b4093174c0d33941465d7cd486d98ce54
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 16:22:14 +0530

    First Commit

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,12 +1,12 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text } from 'react-native';
-import { User } from '../types/UserTypes';
-import { SearchRequest, SearchResponse } from '../apis/SearchApi';
+import { View, Text, TextInput, Button } from 'react-native';
+import { SearchRequest, SearchResponse } from '../types/SearchTypes';
+import { searchApi } from '../apis/SearchApi';
 
 const SearchScreen: React.FC = () => {
   const [keyword, setKeyword] = useState('');
-  const [searchResults, setSearchResults] = useState<User[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResponse[]>([]);
 
   const handleSearch = async () => {
     try {
@@ -14,36 +14,30 @@ const SearchScreen: React.FC = () => {
         keyword: keyword,
       };
 
-      // Make API call to search for specific information
-      const response: SearchResponse = await searchApi(request);
-
+      const response = await searchApi(request);
       setSearchResults(response.searchResults);
     } catch (error) {
-      console.error('Error searching for information:', error);
+      console.error('Error searching:', error);
     }
   };
 
   return (
     <View>
+      <Text>Search Screen</Text>
       <TextInput
         placeholder="Enter keyword"
         value={keyword}
         onChangeText={setKeyword}
       />
       <Button title="Search" onPress={handleSearch} />
-
-      <FlatList
-        data={searchResults}
-        keyExtractor={(item) => item.userId}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.firstName} {item.lastName}</Text>
-            <Text>{item.email}</Text>
-            <Text>{item.phone}</Text>
-            <Text>{item.address}</Text>
-          </View>
-        )}
-      />
+      {searchResults.map((result) => (
+        <View key={result.userId}>
+          <Text>{result.firstName} {result.lastName}</Text>
+          <Text>Email: {result.email}</Text>
+          <Text>Phone: {result.phone}</Text>
+          <Text>Address: {result.address}</Text>
+        </View>
+      ))}
     </View>
   );
 };
